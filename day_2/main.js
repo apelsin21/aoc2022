e70c4e0be67f440cc1a4a9f2b5b04c4b6fe03e3a
Axel Vestin <apelsin12@protonmail.ch>
2022-12-02 19:48:31 +0100

    oops råkade skriva över

--- day_2/main.js
+++ day_2/main.js
@@ -1,1 +1,31 @@
-console.log(require('fs').readFileSync('input', 'utf8').split('\n').slice(0, -1).map(x => [x[0].charCodeAt()-64, x[2].charCodeAt()-87]).reduce((a, b) => a += (b[0] == b[1] ? Math.abs(b[0] - 4) : (b[0]+b[1]) - 2) + (b[1] - 1)*3, 0));
+const fs = require('fs');
+const filename = 'input';
+
+var banan = fs.readFileSync(filename, 'utf8').split('\n');
+banan.pop();
+
+var player_move_score = {
+    'X': 1, //ROCK
+    'Y': 2, //PAPER
+    'Z': 3, //SCISSORS
+};
+
+var outcome_score = {
+    'X': 0, //LOSE
+    'Y': 3, //DRAW
+    'Z': 6, //WIN
+};
+
+var score_map = {
+    'A X': player_move_score['Z'] + outcome_score['X'], //ROCK LOSE SCISSORS
+    'A Y': player_move_score['X'] + outcome_score['Y'], //ROCK DRAW ROCK 
+    'A Z': player_move_score['Y'] + outcome_score['Z'], //ROCK WIN PAPER
+    'B X': player_move_score['X'] + outcome_score['X'], //PAPER LOSE ROCK
+    'B Y': player_move_score['Y'] + outcome_score['Y'], //PAPER DRAW PAPER
+    'B Z': player_move_score['Z'] + outcome_score['Z'], //PAPER WIN SCISSORS
+    'C X': player_move_score['Y'] + outcome_score['X'], //SCISSORS LOSE PAPER
+    'C Y': player_move_score['Z'] + outcome_score['Y'], //SCISSORS DRAW SCISSORS
+    'C Z': player_move_score['X'] + outcome_score['Z'], //SCISSORS WIN ROCK
+};
+
+console.log(banan.reduce((a, b) => a += score_map[b], 0));
