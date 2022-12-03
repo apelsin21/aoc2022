6169976cd420a817c9e5c137859b9f8dee7080f1
Axel Vestin <apelsin12@protonmail.ch>
2022-12-03 17:06:16 +0100

    shorter golf

--- day_3/golf.js
+++ day_3/golf.js
@@ -1,5 +1,2 @@
-function char_to_prio(ascii) {
-    return ascii >= 97 ? ascii - 96 : ascii - 38;
-}
-
-console.log(require('fs').readFileSync('input', 'utf8').split('\n').slice(0,-1).reduce((a,b,i,all) => (i+1)%3==0 ? a += char_to_prio([...b].find(c => all[i-2].indexOf(c) != -1 && all[i-1].indexOf(c) != -1).charCodeAt()) : a, 0));
+var d = '';
+console.log(require('fs').readFileSync('input', 'utf8').split('\n').slice(0,-1).reduce((a,b,i,all) => (i+1)%3==0 ? a += ((d = [...b].find(c => all[i-2].indexOf(c) != -1 && all[i-1].indexOf(c) != -1).charCodeAt()) - (d > 96 ? 96 : 38)) : a, 0));
